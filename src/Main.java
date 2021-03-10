15b1f594f731808b493536eeba7cd0f048e5139f
Alex Makarov <alexpg27@yandex.ru>
2021-03-10 12:41:50 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientCurrentAccount = 100;
+        int accountReplenishment = 1100;
+        int clientBonuses = 0;
+
+        if (accountReplenishment < 1000) {
+            clientCurrentAccount += accountReplenishment;
+            System.out.println("Итоговый бонус = " + clientBonuses);
+        } else if (accountReplenishment > 1000) {
+            clientBonuses = accountReplenishment / 100;
+            clientCurrentAccount += accountReplenishment + clientBonuses;
+            System.out.println("Итоговый бонус = " + clientBonuses);
+        }
+
+
+    }
+}
